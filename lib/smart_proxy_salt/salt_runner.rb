9b1612d995c659b192c211f1e593f902951beadc
Adam Ruzicka <aruzicka@redhat.com>
2021-05-06 15:02:48 +0200

    Fixes #32547 - Make rubocop happy

--- lib/smart_proxy_salt/salt_runner.rb
+++ lib/smart_proxy_salt/salt_runner.rb
@@ -1,52 +1,54 @@
 require 'foreman_tasks_core/runner/command_runner'
 
-module Proxy::Salt
-  # Implements the SaltRunner to be used by foreman_remote_execution
-  class SaltRunner < ForemanTasksCore::Runner::CommandRunner
-    DEFAULT_REFRESH_INTERVAL = 1
+module Proxy
+  module Salt
+    # Implements the SaltRunner to be used by foreman_remote_execution
+    class SaltRunner < ForemanTasksCore::Runner::CommandRunner
+      DEFAULT_REFRESH_INTERVAL = 1
 
-    attr_reader :jid
+      attr_reader :jid
 
-    def initialize(options, suspended_action)
-      super(options, :suspended_action => suspended_action)
-      @options = options
-    end
-
-    def start
-      command = generate_command
-      logger.debug("Running command '#{command.join(' ')}'")
-      initialize_command(*command)
-    end
-
-    def kill
-      publish_data('== TASK ABORTED BY USER ==', 'stdout')
-      publish_exit_status(1)
-      ::Process.kill('SIGTERM', @command_pid)
-    end
-
-    def publish_data(data, type)
-      if @jid.nil? && (match = data.match(/jid: ([0-9]+)/))
-        @jid = match[1]
+      def initialize(options, suspended_action)
+        super(options, :suspended_action => suspended_action)
+        @options = options
       end
-      super
-    end
 
-    def publish_exit_status(status)
-      # If there was no salt job associated with this run, mark the job as failed
-      status = 1 if @jid.nil?
-      super status
-    end
+      def start
+        command = generate_command
+        logger.debug("Running command '#{command.join(' ')}'")
+        initialize_command(*command)
+      end
 
-    private
+      def kill
+        publish_data('== TASK ABORTED BY USER ==', 'stdout')
+        publish_exit_status(1)
+        ::Process.kill('SIGTERM', @command_pid)
+      end
 
-    def generate_command
-      saltfile_path = ::Proxy::Salt::Plugin.settings[:saltfile]
-      command = %w[salt --show-jid]
-      command << "--saltfile=#{saltfile_path}" if File.file?(saltfile_path)
-      command << @options['name']
-      command << 'state.template_str'
-      command << @options['script']
-      command
+      def publish_data(data, type)
+        if @jid.nil? && (match = data.match(/jid: ([0-9]+)/))
+          @jid = match[1]
+        end
+        super
+      end
+
+      def publish_exit_status(status)
+        # If there was no salt job associated with this run, mark the job as failed
+        status = 1 if @jid.nil?
+        super status
+      end
+
+      private
+
+      def generate_command
+        saltfile_path = ::Proxy::Salt::Plugin.settings[:saltfile]
+        command = %w[salt --show-jid]
+        command << "--saltfile=#{saltfile_path}" if File.file?(saltfile_path)
+        command << @options['name']
+        command << 'state.template_str'
+        command << @options['script']
+        command
+      end
     end
   end
 end
--- lib/smart_proxy_salt/salt_task_launcher.rb
+++ lib/smart_proxy_salt/salt_task_launcher.rb
@@ -1,23 +1,25 @@
-module Proxy::Salt
-  # Implements the TaskLauncher::Batch for Salt
-  class SaltTaskLauncher < ForemanTasksCore::TaskLauncher::Batch
-    # Implements the Runner::Action for Salt
-    class SaltRunnerAction < ForemanTasksCore::Runner::Action
-      def initiate_runner
-        additional_options = {
-          :step_id => run_step_id,
-          :uuid => execution_plan_id
-        }
-        ::Proxy::Salt::SaltRunner.new(
-          input.merge(additional_options),
-          :suspended_action => suspended_action
-        )
+module Proxy
+  module Salt
+    # Implements the TaskLauncher::Batch for Salt
+    class SaltTaskLauncher < ForemanTasksCore::TaskLauncher::Batch
+      # Implements the Runner::Action for Salt
+      class SaltRunnerAction < ForemanTasksCore::Runner::Action
+        def initiate_runner
+          additional_options = {
+            :step_id => run_step_id,
+            :uuid => execution_plan_id
+          }
+          ::Proxy::Salt::SaltRunner.new(
+            input.merge(additional_options),
+            :suspended_action => suspended_action
+          )
+        end
       end
-    end
 
-    def child_launcher(parent)
-      ForemanTasksCore::TaskLauncher::Single.new(world, callback, :parent => parent,
-                                                                  :action_class_override => SaltRunnerAction)
+      def child_launcher(parent)
+        ForemanTasksCore::TaskLauncher::Single.new(world, callback, :parent => parent,
+                                                                    :action_class_override => SaltRunnerAction)
+      end
     end
   end
 end
